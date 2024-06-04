1702ff2c130963178344465911fe42f7345dd9a9
Karol Sowiński <92691232+karolsowi@users.noreply.github.com>
2024-06-04 16:40:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render((
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>
 );
 reportWebVitals();
