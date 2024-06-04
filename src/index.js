3e9e94ebf6b7922135480e6b5f7670ad885b6789
Karol <s210939@sggw.edu.pl>
2024-06-04 17:33:01 +0200

    private

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render((
    <BrowserRouter basename={process.env.PUBLIC_URL}>
      <App />
    </BrowserRouter>
-);
+), document.getElementById('root'));
 reportWebVitals();
