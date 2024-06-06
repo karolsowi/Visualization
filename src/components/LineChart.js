8b06b5bb56c2be1f437996febf7fd3d9e99a26a1
Sara Stec <sara.stec1002@gmail.com>
2024-06-06 09:58:24 +0200

    line chart fixes

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -116,7 +116,7 @@ const LineChart = () => {
     markers: {
       size: 3,
       hover: {
-        size: 1,
+        size: 4,
         sizeOffset: 6
       }
     },
@@ -197,134 +197,30 @@ const LineChart = () => {
 
   const series = [
     {
-        name: 'edukacja',
-        data: [
-            { x: '2018', y: '54928' },
-            { x: '2019', y: '49844' },
-            { x: '2020', y: '47629' },
-            { x: '2021', y: '44981' },
-            { x: '2022', y: '41433' }
-        ],
-        group: 'apexcharts-axis-0',
-        zIndex: 0,
-        color: '#000000' 
+      name: 'technika, przemysł i budownictwo',
+      data: [
+          { x: '2018', y: '200889' },
+          { x: '2019', y: '178890' },
+          { x: '2020', y: '171939' },
+          { x: '2021', y: '161730' },
+          { x: '2022', y: '153326' }
+      ],
+      group: 'apexcharts-axis-0',
+      zIndex: 7,
+      color: "#cd79a6"
     },
     {
-        name: 'indywidualne studia międzyobszarowe',
-        data: [
-            { x: '2018', y: '2027' },
-            { x: '2019', y: '21537' },
-            { x: '2020', y: '18867' },
-            { x: '2021', y: '15267' },
-            { x: '2022', y: '15126' }
-        ],
-        group: 'apexcharts-axis-0',
-        zIndex: 1,
-        color: '#e79f01'
-    },
-    {
-        name: 'nauki przyrodnicze, matematyka i statystyka',
-        data: [
-            { x: '2018', y: '47182' },
-            { x: '2019', y: '44621' },
-            { x: '2020', y: '43575' },
-            { x: '2021', y: '41551' },
-            { x: '2022', y: '40509' }
-        ],
-        group: 'apexcharts-axis-0',
-        zIndex: 2,
-        color: "#56b5e8"
-    },
-    {
-        name: 'nauki społeczne, dziennikarstwo i informacja',
-        data: [
-            { x: '2018', y: '101129' },
-            { x: '2019', y: '93127' },
-            { x: '2020', y: '96166' },
-            { x: '2021', y: '92687' },
-            { x: '2022', y: '93134' }
-        ],
-        group: 'apexcharts-axis-0',
-        zIndex: 3,
-        color: "#009f72"
-    },
-    {
-        name: 'prowadzenie działalności gospodarczej, administracja i prawo',
-        data: [
-            { x: '2018', y: '157821' },
-            { x: '2019', y: '150743' },
-            { x: '2020', y: '146015' },
-            { x: '2021', y: '143604' },
-            { x: '2022', y: '139668' }
-        ],
-        group: 'apexcharts-axis-0',
-        zIndex: 4,
-        color: "#c4b819"
-    },
-    {
-        name: 'rolnictwo, leśnictwo, rybołówstwo i weterynaria',
-        data: [
-            { x: '2018', y: '23787' },
-            { x: '2019', y: '21517' },
-            { x: '2020', y: '22156' },
-            { x: '2021', y: '21433' },
-            { x: '2022', y: '20399' }
-        ],
-        group: 'apexcharts-axis-0',
-        zIndex: 5,
-        color: "#0073b3"
-    },
-    {
-        name: 'sztuka i przedmioty humanistyczne',
-        data: [
-            { x: '2018', y: '95519' },
-            { x: '2019', y: '91175' },
-            { x: '2020', y: '92898' },
-            { x: '2021', y: '90415' },
-            { x: '2022', y: '89523' }
-        ],
-        group: 'apexcharts-axis-0',
-        zIndex: 6,
-        color: "#d45e01"
-    },
-    {
-        name: 'technika, przemysł i budownictwo',
-        data: [
-            { x: '2018', y: '200889' },
-            { x: '2019', y: '178890' },
-            { x: '2020', y: '171939' },
-            { x: '2021', y: '161730' },
-            { x: '2022', y: '153326' }
-        ],
-        group: 'apexcharts-axis-0',
-        zIndex: 7,
-        color: "#cd79a6"
-    },
-    {
-        name: 'technologie informacyjno-komunikacyjne',
-        data: [
-            { x: '2018', y: '53787' },
-            { x: '2019', y: '51010' },
-            { x: '2020', y: '51890' },
-            { x: '2021', y: '51261' },
-            { x: '2022', y: '51202' }
-        ],
-        group: 'apexcharts-axis-0',
-        zIndex: 8,
-        color: "#999999"
-    },
-    {
-        name: 'usługi',
-        data: [
-            { x: '2018', y: '55028' },
-            { x: '2019', y: '51357' },
-            { x: '2020', y: '49007' },
-            { x: '2021', y: '45475' },
-            { x: '2022', y: '44235' }
-        ],
-        group: 'apexcharts-axis-0',
-        zIndex: 9,
-        color: "#146a2e"
+      name: 'prowadzenie działalności gospodarczej, administracja i prawo',
+      data: [
+          { x: '2018', y: '157821' },
+          { x: '2019', y: '150743' },
+          { x: '2020', y: '146015' },
+          { x: '2021', y: '143604' },
+          { x: '2022', y: '139668' }
+      ],
+      group: 'apexcharts-axis-0',
+      zIndex: 4,
+      color: "#c4b819"
     },
     {
       name: 'zdrowie i opieka społeczna',
@@ -338,7 +234,111 @@ const LineChart = () => {
       group: 'apexcharts-axis-0',
       zIndex: 10,
       color: "#b001ff"
-  }
+    },
+    {
+      name: 'nauki społeczne, dziennikarstwo i informacja',
+      data: [
+          { x: '2018', y: '101129' },
+          { x: '2019', y: '93127' },
+          { x: '2020', y: '96166' },
+          { x: '2021', y: '92687' },
+          { x: '2022', y: '93134' }
+      ],
+      group: 'apexcharts-axis-0',
+      zIndex: 3,
+      color: "#009f72"
+    },
+    {
+      name: 'sztuka i przedmioty humanistyczne',
+      data: [
+          { x: '2018', y: '95519' },
+          { x: '2019', y: '91175' },
+          { x: '2020', y: '92898' },
+          { x: '2021', y: '90415' },
+          { x: '2022', y: '89523' }
+      ],
+      group: 'apexcharts-axis-0',
+      zIndex: 6,
+      color: "#d45e01"
+    },
+    {
+      name: 'technologie informacyjno-komunikacyjne',
+      data: [
+          { x: '2018', y: '53787' },
+          { x: '2019', y: '51010' },
+          { x: '2020', y: '51890' },
+          { x: '2021', y: '51261' },
+          { x: '2022', y: '51202' }
+      ],
+      group: 'apexcharts-axis-0',
+      zIndex: 8,
+      color: "#999999"
+    },
+    {
+      name: 'usługi',
+      data: [
+          { x: '2018', y: '55028' },
+          { x: '2019', y: '51357' },
+          { x: '2020', y: '49007' },
+          { x: '2021', y: '45475' },
+          { x: '2022', y: '44235' }
+      ],
+      group: 'apexcharts-axis-0',
+      zIndex: 9,
+      color: "#146a2e"
+    },
+    {
+        name: 'edukacja',
+        data: [
+            { x: '2018', y: '54928' },
+            { x: '2019', y: '49844' },
+            { x: '2020', y: '47629' },
+            { x: '2021', y: '44981' },
+            { x: '2022', y: '41433' }
+        ],
+        group: 'apexcharts-axis-0',
+        zIndex: 0,
+        color: '#000000' 
+    },
+    {
+      name: 'nauki przyrodnicze, matematyka i statystyka',
+      data: [
+          { x: '2018', y: '47182' },
+          { x: '2019', y: '44621' },
+          { x: '2020', y: '43575' },
+          { x: '2021', y: '41551' },
+          { x: '2022', y: '40509' }
+      ],
+      group: 'apexcharts-axis-0',
+      zIndex: 2,
+      color: "#56b5e8"
+    },
+    {
+      name: 'rolnictwo, leśnictwo, rybołówstwo i weterynaria',
+      data: [
+          { x: '2018', y: '23787' },
+          { x: '2019', y: '21517' },
+          { x: '2020', y: '22156' },
+          { x: '2021', y: '21433' },
+          { x: '2022', y: '20399' }
+      ],
+      group: 'apexcharts-axis-0',
+      zIndex: 5,
+      color: "#0073b3"
+    },
+    {
+        name: 'indywidualne studia międzyobszarowe',
+        data: [
+            { x: '2018', y: '2027' },
+            { x: '2019', y: '21537' },
+            { x: '2020', y: '18867' },
+            { x: '2021', y: '15267' },
+            { x: '2022', y: '15126' }
+        ],
+        group: 'apexcharts-axis-0',
+        zIndex: 1,
+        color: '#e79f01'
+    }
 ];
 
   return (
